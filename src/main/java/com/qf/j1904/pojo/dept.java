c896ed1788f9a78bd17ff163039b25d7f0426a77
hjn-y <395256150@qq.com>
2019-08-22 16:25:51 +0800

    abc

--- src/main/java/com/qf/j1904/pojo/dept.java
+++ src/main/java/com/qf/j1904/pojo/dept.java
@@ -1,8 +1,8 @@
 package com.qf.j1904.pojo;
 
 public class dept {
-    private int deptno;
-    private String dname;
-    private String loc;
+    private int deptno; //部门编号
+    private String dname; //部门名称
+    private String loc; //部门地址
 
 }
